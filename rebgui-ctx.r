2e679def23d0a036a83ceefe6b48e920d0b4fa22
Semseddin <semseddin@gmail.com>
2013-09-19 11:36:40 -0700

    Added ENABLE and DISABLE functions.

--- rebgui-ctx.r
+++ rebgui-ctx.r
@@ -1077,6 +1077,60 @@ ctx-rebgui: make object! [
 	] in system/view 'self
 ]
 
+enable: make function! [
+	"Enable a widget."
+	face [object! block!]
+] [
+	foreach f reduce either object? face [[face]] [face] [
+		if 'disable = f/parent-face/type [
+			f: f/parent-face
+			f/pane/1/offset: f/offset
+			f/pane/1/span: f/data
+			change find f/parent-face/pane f f/pane/1
+			show f/parent-face
+		]
+	]
+]
+
+disable: make function! [
+	"Disable a widget."
+	face [object! block!]
+] [
+	foreach f reduce either object? face [[face]] [face] [
+		unless 'disable = f/parent-face/type [
+			change find f/parent-face/pane f make ctx-rebgui/rebface [
+				type: 'disable
+				offset: f/offset
+				size: f/size
+				span: all [f/span copy f/span]
+				pane: reduce [
+					f
+					make ctx-rebgui/rebface [
+						size: f/size
+						span: all [
+							f/span
+							case [
+								all [find f/span #"H" find f/span #"H"] [#HW]
+								find f/span #"H" [#H]
+								find f/span #"W" [#W]
+							]
+						]
+						effect: [merge colorize 224.224.224]
+					]
+				]
+				data: all [f/span copy f/span]
+				feel: action: none
+			]
+			f/offset: 0x0
+			if f/span [
+				remove find f/span #"X"
+				remove find f/span #"Y"
+			]
+			show f/parent-face
+		]
+	]
+]
+
 ;	add widget names to words
 insert tail ctx-rebgui/words ctx-rebgui/widget-names: copy find first ctx-rebgui/widgets 'anim
 
